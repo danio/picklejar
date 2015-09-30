e41adf555ed3449e0b195847c879771e2edf3556
danio <dan.pidcock@googlemail.com>
2015-09-30 08:34:44 +0100

    Make it work in nw.js 0.13

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-var runtests = require('runtests');
-var output = require('outputdocument');
+try {
+if (nw)
+{
+  // nw.js 0.13 or later
+  cwd = function() { return nw.process.cwd(); }
+}
+} catch(e) {
+  // nw.js 0.12
+  cwd = function() { return process.cwd(); }
+  nw = {
+    require: function(module) { return require(module);}
+  };
+}
+
+var runtests = nw.require('runtests');
+var output = nw.require('outputdocument');
+
 
 function runTests() {
   runtests.runTests(['features'], output, document);
