c450ee4518983d43e2342ed916e6b9af65703397
danio <dan.pidcock@googlemail.com>
2015-10-01 08:06:11 +0100

    Allow current file view directory to be chosen

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
+// Make some globals to access nw.js functions in a cross-version way
 try {
-if (nw)
-{
-  // nw.js 0.13 or later
-  cwd = function() { return nw.process.cwd(); }
-}
+  if (nw)
+  {
+    // nw.js 0.13 or later
+    cwd = function() { return nw.process.cwd(); }
+  }
 } catch(e) {
   // nw.js 0.12
   cwd = function() { return process.cwd(); }
@@ -12,14 +13,23 @@ if (nw)
   };
 }
 
+var path = nw.require('path');
 var runtests = nw.require('runtests');
 var output = nw.require('outputdocument');
 var folder_view = nw.require('folder_view');
 
-
 function runTests() {
   runtests.runTests(['features'], output, document);
 }
+
+function fileChanged(filepath) {
+  var dir = path.win32.dirname(filepath); // todo make work cross-platform
+  folder.open(dir);
+}
+
+$('#runTests').on('click', runTests);
+$('#openFile').on('click', function() { $('#folderName').trigger('click'); } );
+$('#folderName').on('change', function() { fileChanged($('#folderName').val()); });
 
 var folder = new folder_view.Folder($('#filebrowser'));
 folder.open(cwd() + '/features');
