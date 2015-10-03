af8e71912df0ed27f66348670f9ac7e92a601a2a
danio <dan.pidcock@googlemail.com>
2015-10-03 22:44:12 +0100

    navigate/run tests on file view double click

--- js/main.js
+++ js/main.js
@@ -36,4 +36,13 @@ $('#folderName').on('change', function() { fileChanged($('#folderName').val()); 
 var folder = new folder_view.Folder($('#filebrowser'));
 folder.open(cwd() + '/features');
 
+folder.on('navigate', function(dir, mime) {
+  if (mime.type === 'folder') {
+    folder.open(dir);
+  } else {
+    runTests();
+  }
+});
+
+
 runTests();
