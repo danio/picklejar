8d5d164a29d7bfbf46cdcc7fcab545f802f024ec
danio <dan.pidcock@googlemail.com>
2015-10-15 08:54:50 +0100

    Wait till document ready before running script

--- js/main.js
+++ js/main.js
@@ -7,14 +7,11 @@ var OutputDocument = nw.require('outputdocument');
 var folder_view = nw.require('folder_view');
 var MruDirCache = nw.require("mru-dir-cache");
 
-var gherkinParser = GherkinParser();
-var output = OutputDocument(gherkinParser);
-
-function runTests(testPaths) {
+function runTests(testPaths, output) {
   runtests.runTests(testPaths, output);
 }
 
-function fileChanged(filepath) {
+function fileChanged(filepath, folder, cache) {
   if (filepath) {
     var dir = path.dirname(filepath);
     cache.store(dir, '');
@@ -22,35 +19,41 @@ function fileChanged(filepath) {
   }
 }
 
-$('#runAllTests').on('click', function() { runTests([folder.dir]); });
-$('#openFile').on('click', function() { $('#folderName').trigger('click'); });
-$('#folderName').on('change', function() { fileChanged($('#folderName').val()); });
+$(document).ready(function() {
+  var cache = MruDirCache({ max: 10 }, localStorage);
+  var gherkinParser = GherkinParser();
+  var output = OutputDocument(gherkinParser);
+  var folder = new folder_view.Folder($('#filebrowser'));
 
-var folder = new folder_view.Folder($('#filebrowser'));
+  $('#runAllTests').on('click', function() { runTests([folder.dir], output); });
+  // The file browse button is pressed
+  $('#openFile').on('click', function() { $('#folderName').trigger('click'); });
+  // The file browse action is completed
+  $('#folderName').on('change', function() { fileChanged($('#folderName').val(), folder, cache); });
 
-folder.on('navigate', function(path, mime) {
-  if (mime.type === 'folder') {
-    folder.open(path);
-  } else {
-    runTests([path]);
+  // callback from folder_view when a file is double clicked on
+  folder.on('navigate', function(path, mime) {
+    if (mime.type === 'folder') {
+      folder.open(path);
+    } else {
+      runTests([path], output);
+    }
+  });
+
+  var latest = cache.latest();
+  if (latest) {
+    folder.open(latest);
+    cache.cache_.keys().forEach( function(x) { $('#dirlist').append('<option>' + x + '</option>'); });
+    // The folder drop-down list is clicked on
+    $('#dirlist').on('change', function(e) {
+      dir = e.target.selectedOptions[0].text;
+      cache.store(dir, '');
+      folder.open(dir);
+    });
+    runTests([folder.dir], output);
+  }
+  else {
+    // first time app has been run
+    $('#folderName').trigger('click'); // open file browser
   }
 });
-
-function navigate(e) {
-  dir = e.target.selectedOptions[0].text;
-  cache.store(dir, '');
-  folder.open(dir);
-}
-
-var cache = MruDirCache({ max: 10 }, localStorage);
-var latest = cache.latest();
-if (latest) {
-  folder.open(latest);
-  cache.cache_.keys().forEach( function(x) { $('#dirlist').append('<option>' + x + '</option>'); });
-  $('#dirlist').on('change', navigate);
-  runTests([folder.dir]);
-}
-else {
-  // first time app has been run
-  $('#folderName').trigger('click'); // open file browser
-}
